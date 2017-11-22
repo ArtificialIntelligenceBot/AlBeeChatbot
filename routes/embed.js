aa7e13c21492175d73af92c74b522083049738a3
Max Tsai <mt8168@gmail.com>
2017-11-22 08:48:39 -0800

    EMBED view Update

--- routes/embed.js
+++ routes/embed.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/Example', function(req, res, next) {
+/* GET Main page. */
+router.get('/', function(req, res, next) {
 
+  res.render('embed', {
+    title: 'AlBeeChatbot',
+    messageoftheday: 'Weclome to AlBee Chatbot - your Smart Virtual Assistant!'
+  });
+
+});
+
+/* GET Example page. */
+router.get('/Example', function(req, res, next) {
 
   res.render('embedExample', {
     title: 'AlBeeChatbot',
